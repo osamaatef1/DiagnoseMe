68c4749dbcfbcf122a95874d2a1e1b4fce89f3f5
osamaatef1 <osamaatef111@gmail.com>
2023-06-10 14:08:06 +0300

    Updated

--- app/Traits/ImageUploader.php
+++ app/Traits/ImageUploader.php
@@ -11,12 +11,9 @@ trait ImageUploader
         $image = $request->file($key);
         $img = time()  . rand(1000 , 20000) . "." . $image->getClientOriginalExtension();
         $image->move(storage_path("app/public/" . $placeMove) , $img);
-        $model->update([$attribute => $placeMove . $img]);
-    }
-       public function uploadimg(\Illuminate\Support\Facades\Request $request){
-        $image = $request->file('image')->getClientOriginalExtension();
-        $path = $request->file("image")->storeAs('users',$image,'');
+        $model->update([$attribute => $placeMove ."/" . $img]);
 
     }
+
 
 }
